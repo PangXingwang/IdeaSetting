ed45a8be40ff788299e56ad0617382d8934ae031
pangxingwang <pangxingwang@ybm100.com>
2020-12-07 09:45:41 +0800

    IU-2020.3 <pangxingwang@a0101015710 Create debugger.xml, lombok-plugin.xml, colors.scheme.xml, databaseDrivers.xml, _@user_Default.icls, updates.xml, MyBatisCodeHelper.xml, Default copy.xml, keymap.xml, github.xml, editor.xml, duplocatorSettings.xml, Default.xml, ide.general.xml, project.default.xml, baseRefactoring.xml, Default copy.icls, shared-indexes.xml, wsl.distributions.xml, pluginAdvertiser.xml, git_toolbox_2.xml, path.macros.xml, toStringTemplates.xml, laf.xml, ui.lnf.xml, web-browsers.xml, git.xml, usageView.xml, jprofiler.xml, ide-features-trainer.xml, Default.xml, OGNL _Struts 2_.xml, userDefine.xml, yiiguxing.translation.xml, Class.java, jdk.table.xml, notifications.xml, vim_settings.xml, filetypes.xml, vcs.xml, terminal.xml, OGNL.xml, javarebel.xml, templates.xml, find.xml, diff.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ *<p></p> 
+ *
+ *@author pangxingwang
+ *@Date: ${DATE}
+ */
+public class ${NAME} {
+}
